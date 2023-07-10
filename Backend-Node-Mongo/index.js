e1c8c2e9ebe8092a428ef6e1cf4bc5d7ddc793a5
jogebro <joelabril2110@gmail.com>
2023-07-10 13:12:28 -0500

    feat: :tada: Inicio Get

---
+++ Backend-Node-Mongo/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import conectarDB from "./config/config.js";
+import categoriasRouter from "./routes/categorias.routes.js";
+
+const app = express();
+dotenv.config();
+
+app.use("/categorias", categoriasRouter);
+
+const PORT = process.env.PORT
+
+conectarDB();
+
+app.listen(PORT, ()=>{
+    console.log(`El server esta corriendo en el puerto ${PORT}`);
+});
+
